87612fb4b9ec4a5ce7253f9385dcf171dc755806
Reid <rreid112@gmail.com>
2022-02-19 15:38:01 -0600

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+    <header>
+        <nav>
+            <p class="text-3xl text-blue-500">Navigation will go here</p>
+        </nav>
+    </header>
+
+    <main>
+        <RouterView />
+    </main>
+
+    <footer>
+        <p>Footer will go here</p>
+    </footer>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+    <main>
+        <p>Home View</p>
+    </main>
+</template>
